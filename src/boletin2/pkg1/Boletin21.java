489a605e7732bf9247da348f79bd888f9f7238b3
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-10-09 08:51:03 +0200

    inicio

---
+++ src/boletin2/pkg1/Boletin21.java
@@ -0,0 +1,19 @@
+
+package boletin2.pkg1;
+
+/**
+ * @author jvidalmoreira
+ */
+public class Boletin21 {
+
+    public static void main(String[] args) {
+      float b = 4f, h = 3f,area;
+        area = b * h /2;
+        System.out.println("area ="+area);
+        
+        
+        
+        
+    }
+    
+}
